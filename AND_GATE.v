c9324b93d13c281052cb505a81fdc12a25da15dd
Manu Emmanuel <manuemmanuel228@gmail.com>
2024-04-15 11:00:48 +0530

    Create AND_GATE.v

---
+++ AND_GATE.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: SJCET
+// Engineer: Manu Emmanuel
+// 
+// Create Date:    10:45:16 04/15/2024 
+// Design Name: 
+// Module Name:    AND_Gate 
+// Project Name: Logic_Gates
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module AND_Gate(
+    input a,
+    input b,
+    output y
+    );
+
+and(y,a,b);
+endmodule

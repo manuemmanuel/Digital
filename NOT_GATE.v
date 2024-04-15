0bf2c3d8c6382eee347822a3f4a474de529689d1
Manu Emmanuel <manuemmanuel228@gmail.com>
2024-04-15 11:56:58 +0530

    Create NOT_GATE.v

---
+++ NOT_GATE.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:39:12 04/15/2024 
+// Design Name: 
+// Module Name:    NOT_GATE 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module NOT_GATE(
+    input a,
+    output y
+    );
+
+not(y,a);
+endmodule
